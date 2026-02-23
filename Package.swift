3e02188eb705471d2fe2e2399cad0a70f960eb1a
ajc6432 <ajc6432@protonmail.com>
2026-02-22 18:58:46 -0800

    Package.swift adjustment for Pitchy ref

--- Package.swift
+++ Package.swift
@@ -1,16 +1,18 @@
-// swift-tools-version: 6.0
-
+// swift-tools-version:6.0
 import PackageDescription
 
 let package = Package(
     name: "Beethoven",
+    platforms: [.iOS(.v15)],
     products: [
         .library(name: "Beethoven", targets: ["Beethoven"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ajc6432/Pitchy", from: "2.0.0")
+        // users cloning Beethoven will fetch Pitchy automatically
+        .package(url: "https://github.com/ajc6432/Pitchy.git", branch: "master")
     ],
     targets: [
-        .target(name: "Beethoven", dependencies: ["Pitchy"], path: "Source")
+        .target(name: "Beethoven", dependencies: ["Pitchy"], path: "Sources/Beethoven"),
+        .executableTarget(name: "Example", dependencies: ["Beethoven", "Pitchy"], path: "Example")
     ]
 )
